d0b33f4c2708e7179d0bf2f812b0d142871c97bc
Martin Hunt <martin.hunt@adarga.ai>
2020-08-15 18:09:18 +0100

    little bit more styling

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,34 +1,52 @@
 import { gql } from '@apollo/client'
 import styled from 'styled-components'
 
-const ProductCard = ({ product: p }) => {  
-  const image = p.images.edges.length && p.images.edges[0].node
+const ProductCard = ({ product }) => {  
+  const { 
+    images,
+    priceRange: { minVariantPrice, maxVariantPrice },
+    title,
+    description,
+    variants
+  } = product
+
+  const image = images.edges.length && images.edges[0].node
+  
+  const price = minVariantPrice.amount === maxVariantPrice.amount
+    ? `£${parseFloat(minVariantPrice.amount)}`
+    : `£${parseFloat(minVariantPrice.amount)} - ${parseFloat(maxVariantPrice.amount)}`
 
   return (
     <article>
-      {image 
-        ? <Image src={image.transformedSrc} alt={image.altText} />
-        : <Image src={'/product-placeholder.jpg'} alt="placeholder image" />
-      }
-      <h3>{p.title}</h3>
-      <strong>{p.variants.edges[0].node.priceV2.amount}</strong>
+      <ImageContainer>
+        {image ? <img src={image.transformedSrc} alt={image.altText} /> : null}
+      </ImageContainer>
+      <h3>{title}</h3>
+      <strong>{price}</strong>
+      <span>{description}</span>
     </article>
   )
 }
 
-const Image = styled.img`
-  width: 100%;
+const ImageContainer = styled.div`
+  height: 340px;
+  background-color: ${({ theme }) => theme.colors.medGrey};
+  background-image: url('/product-placeholder.png');
+  background-position: center;
+  background-repeat: no-repeat;
+  
+  img { 
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+  }
 `
 
 export const PRODUCT_CARD_FRAGMENT = gql`
   fragment ProductCardFragment on Product {
-    id
     handle 
     title
-    productType
-    vendor
     description(truncateAt: 120)
-    descriptionHtml
     images(first: 1) {
       edges {
         node {
@@ -40,27 +58,6 @@ export const PRODUCT_CARD_FRAGMENT = gql`
     priceRange {
       minVariantPrice { amount currencyCode }
       maxVariantPrice { amount currencyCode }
-    }
-    metafields(first: 250) {
-      edges {
-        node {
-          description
-          value
-          valueType
-        }
-      }
-    }
-    variants(first: 250) {
-      edges {
-        node {
-          available
-          availableForSale
-          priceV2{ amount, currencyCode }
-          id
-          sku
-          title
-        }
-      }
     }
   }
 `
